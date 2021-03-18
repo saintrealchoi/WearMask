9549a55e5a12f0dc5cf73d38fa45face9ead920b
sungjinchoi <csj7480@gmail.com>
2021-03-18 23:27:39 +0900

    Create wearmask.cmd

---
+++ wearmask.cmd
@@ -0,0 +1,7 @@
+@echo off
+cd c:/projects/wearmask
+# ------------------------- [edit] ------------------------ #
+set FLASK_APP=wearmask
+set FLASK_ENV=development
+# --------------------------------------------------------- #
+c:/venvs/wearmask/scripts/activate
